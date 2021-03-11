6486b9225f89598653cce0efa61ffae8f70d5c2a
petter0619 <petter0619@gmail.com>
2021-03-11 09:58:54 +0100

    AddMultipleChoiceQuestionForm

--- client/src/components/AddMultipleChoiceQuestionForm.js
+++ client/src/components/AddMultipleChoiceQuestionForm.js
@@ -1,9 +1,40 @@
 import React from 'react';
+// Helpers
+import { shuffleArray } from '../helpers';
 
-export default function AddMultipleChoiceQuestionForm() {
+export default function AddMultipleChoiceQuestionForm({addQuestion, setShowMcForm}) {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const question = {
+            type: 'multiple choice',
+            question: e.target.question.value,
+            answer: e.target.answer.value,
+            multipleChoices: shuffleArray([
+                e.target.answer.value,
+                e.target.choice1.value,
+                e.target.choice2.value,
+            ]),
+        }
+        addQuestion(question);
+        e.target.reset();
+        setShowMcForm(false);
+    }
+    
     return (
-        <div>
-            Multiple Choice Form component
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="question">Enter your question:</label>
+            <input type="text" name="question" id="question" required/>
+            <br />
+            <label htmlFor="answer">What the answer is:</label>
+            <input type="text" name="answer" id="answer" required/>
+            <br />
+            <label htmlFor="choice1">Multiple choice option:</label>
+            <input type="text" name="choice1" id="choice1" required/>
+            <br />
+            <label htmlFor="choice2">Multiple choice option:</label>
+            <input type="text" name="choice2" id="choice2" required/>
+
+            <button type="submit">Add Question</button>
+        </form>
     )
 }
