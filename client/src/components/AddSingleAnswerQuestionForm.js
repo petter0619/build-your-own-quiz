83e52d592aa09d6e6968b2a7359aa74077afa24c
petter0619 <petter0619@gmail.com>
2021-03-11 09:59:01 +0100

    AddSingleQuestionForm

--- client/src/components/AddSingleAnswerQuestionForm.js
+++ client/src/components/AddSingleAnswerQuestionForm.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export default function AddSingleAnswerQuestionForm() {
+export default function AddSingleAnswerQuestionForm({addQuestion, setShowSaForm}) {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const question = {
+            type: 'single answer',
+            question: e.target.question.value,
+            answer: e.target.answer.value,
+        }
+        addQuestion(question);
+        e.target.reset();
+        setShowSaForm(false);
+    }
+
     return (
-        <div>
-            Single Answer Question component
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="question">Enter your question:</label>
+            <input type="text" name="question" id="question" required/>
+            <br />
+            <label htmlFor="answer">What the answer is:</label>
+            <input type="text" name="answer" id="answer" required/>
+            <button type="submit">Add Question</button>
+        </form>
     )
 }
